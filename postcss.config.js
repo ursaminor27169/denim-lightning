8472a12ded80df6158600e0aef133be9aff00c60
Ekaterina <katya-polyakova-2429@mail.ru>
2021-01-28 15:32:51 +0300

    fix postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,5 @@
 const config = {
-  plugins: [
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          require('@fullhuman/postcss-purgecss')({
-            content: ['./src/**/*.html'],
-          }),
-          require('cssnano')({
-            preset: 'default',
-          }),
-        ]
-      : []),
-  ],
+  plugins: [require('autoprefixer')],
 }
 
 module.exports = config
